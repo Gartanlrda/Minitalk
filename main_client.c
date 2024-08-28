f11c0f6067f304864834ff6de6d70488f64613a5
ggoy <gaet.jam@gmail.com>
2024-08-28 16:02:40 +0200

    Enfin

--- main_client.c
+++ main_client.c
@@ -6,7 +6,7 @@
 /*   By: ggoy <ggoy@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 16:38:13 by ggoy              #+#    #+#             */
-/*   Updated: 2024/08/27 18:53:01 by ggoy             ###   ########.fr       */
+/*   Updated: 2024/08/28 15:58:26 by ggoy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,11 @@
 
 bool	g_run = false;
 
-// static void	ft_wait_signal(int sign, siginfo_t *info, void *context)
-// {
-// 	info = 0;
-// 	(void)context;
-// 	ft_printf("bjr\n");
-// 	if (g_run == false || sign == SIGUSR1)
-// 		g_run = true;
-// 	// else
-// 	// 	g_run = false;
-// }
-
 static void	encode(int pid, char c)
 {
-	int	i = 0;	
+	int	i;	
 
-	if (i == 8)
-		i = 0;
+	i = 0;
 	while (i < 8)
 	{
 		if (c & (0x01 << i))
@@ -39,38 +27,15 @@ static void	encode(int pid, char c)
 			kill(pid, SIGUSR2);
 		i++;
 		while (g_run == false)
-		{
 			usleep(200);
-			;
-			// sigaction(SIGUSR1, &run, NULL);?
-		}
 		g_run = false;
 	}
-
 }
 
-// static void	encode_pid(int pid, char c)
-// {
-// 	static int	i = 0;
-
-// 	printf("%c\n", c);
-// 	if (i == 8)
-// 		i = 0;
-// 	while (i < 8)
-// 	{
-// 		if (c & (0x01 << i))
-// 			kill(pid, SIGUSR1);
-// 		else
-// 			kill(pid, SIGUSR2);
-// 		i++;
-// 		usleep(400);
-// 	}
-// }
 static void	say_ok(int sign, siginfo_t *info, void *context)
 {
 	info = 0;
 	(void)context;
-	
 	if (sign == SIGUSR2)
 	{
 		ft_printf("Message envoye avec succes!\n");
@@ -82,33 +47,24 @@ static void	say_ok(int sign, siginfo_t *info, void *context)
 
 int	main(int argc, char **argv)
 {
-	// char				*client;
 	int					server;
 	int					i;
 	struct sigaction	sign;
-	// struct sigaction	run;
 
 	sign.sa_sigaction = say_ok;
 	sigemptyset(&sign.sa_mask);
 	sign.sa_flags = SA_SIGINFO;
-	// run.sa_sigaction = ft_wait_signal;
-	// sigemptyset(&run.sa_mask);
-	// run.sa_flags = 0;
 	if (argc == 3)
 	{
 		i = -1;
 		server = ft_atoi(argv[1]);
-		// client = ft_itoa(getpid());
-		printf("I am %d\n", getpid());
 		ft_printf("Envoi du message a %d en cours..\n", server);
-		// sigaction(SIGUSR1, &run, NULL);
-		// while (++i < ft_strlens(client))
-		// 	encode_pid(server, client[i]);
 		i = -1;
-		// free(client);
 		sigaction(SIGUSR2, &sign, NULL);
 		sigaction(SIGUSR1, &sign, NULL);
 		while (++i < ft_strlens(argv[2]))
 			encode(server, argv[2][i]);
+		encode(server, '\n');
+		encode(server, '\0');
 	}
 }
--- main_server.c
+++ main_server.c
@@ -6,11 +6,40 @@
 /*   By: ggoy <ggoy@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 14:38:09 by ggoy              #+#    #+#             */
-/*   Updated: 2024/08/27 18:56:28 by ggoy             ###   ########.fr       */
+/*   Updated: 2024/08/28 15:59:59 by ggoy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+char	*g_msg = NULL;
+
+static char	*minitalk_join(char c)
+{
+	int		mallocizer;
+	int		i;
+	char	*result;
+
+	i = 0;
+	if (g_msg == NULL)
+	{
+		result = malloc(sizeof(char) * 2);
+		result[0] = c;
+		result[1] = '\0';
+		return (result);
+	}
+	mallocizer = ft_strlen(g_msg) + 1;
+	result = malloc(sizeof(char) * (mallocizer + 1));
+	while (g_msg[i])
+	{
+		result[i] = g_msg[i];
+		i++;
+	}
+	result[i] = c;
+	result[++i] = '\0';
+	free(g_msg);
+	return (result);
+}
 
 static void	decode(int sign, siginfo_t *info, void *context)
 {
@@ -20,30 +49,24 @@ static void	decode(int sign, siginfo_t *info, void *context)
 
 	(void)context;
 	client = info->si_pid;
-	// printf("pid: %i\n", info->si_uid);
 	if (sign == SIGUSR1)
 		c |= (0x01 << bit);
 	bit++;
 	if (bit == 8)
 	{
-		// if (client < 100000)
-		// {
-		// 	client = client * 10 + (c - 48);
-		// 	c = 0;
-		// 	bit = 0;
-		// 	printf("%i\n", client);
-		// 	return ;
-		// }
+		g_msg = minitalk_join(c);
 		if (c == '\0')
+		{
 			kill(client, SIGUSR2);
-			// client = 0 * kill(client, SIGUSR2);
-		ft_putchar(c);
+			ft_putstr_fd(g_msg, 0);
+			free(g_msg);
+			g_msg = NULL;
+		}
 		c = 0;
 		bit = 0;
 	}
 	usleep(200);
-	if (kill(client, SIGUSR1) < 0)
-		printf("h\n");
+	kill(client, SIGUSR1);
 }
 
 int	main(int argc, char **argv)
